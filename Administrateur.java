9cc90c59fedcffc9d526eb28ca0c751f60241de5
RaSandratra <randrianantenainasandratra@gmail.com>
2025-08-07 15:17:55 +0300

    Delete Administrateur.java

--- Administrateur.java
+++
@@ -1,9 +0,0 @@
-class Administrateur extends Moderateur {
-    public Administrateur(String id, String username, String email) {
-        super(id, username, email);
-    }
-
-    public void supprimerUtilisateur(Groupe groupe, Utilisateur utilisateur) {
-        groupe.supprimerUtilisateur(utilisateur);
-    }
-}
